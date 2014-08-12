9385722cb97a35cc285630b41b10f7980c4a7993
stripealicious <gl.miller@live.com>
2014-08-12 09:32:21 -0600

    game board
    The game board is all layed out, looks ugly but oh well. its only to
    show jamie what it will look like.

--- GhettoClue/GhettoClue/MainWindow.xaml.cs
+++ GhettoClue/GhettoClue/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,5 +25,75 @@ namespace GhettoClue
         {
             InitializeComponent();
         }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+
+
+            ////gameboard.Children.Remove(gameGrid);
+            //gameGrid = new Grid();
+            //gameGrid.Height = gameboard.RenderSize.Height;
+            //gameGrid.Height = gameboard.RenderSize.Width;
+
+            //double rows = 20;
+            //double col = 20;
+
+            ////double merWidth = gameGrid.Width / col;
+            ////double merHeight = gameGrid.Height / rows;
+
+            ////create columns
+            //int cnum = 0;
+            //for(int i =0; i < col; i++)
+            //{
+            //    ColumnDefinition columns = new ColumnDefinition();
+            //    columns.Width = new GridLength(col);
+            //    gameGrid.ColumnDefinitions.Add(columns);
+            //}
+            //cnum = (int)col;
+            //numcol = cnum;
+
+            ////create rows
+            //int r = 0;for (int i = 0; i < rows; i++)
+            //{
+            //    RowDefinition ro = new RowDefinition();
+            //    ro.Height = new GridLength(rows);
+            //    gameGrid.RowDefinitions.Add(ro);
+            //}
+            //r = (int)rows;
+            //numrow = r;
+
+            ////makes fill and outline color
+            //SolidColorBrush line = new SolidColorBrush();
+            //line.Color = Color.FromArgb(160, 0, 0, 0);
+            //SolidColorBrush fill = new SolidColorBrush();
+            //fill.Color = Color.FromRgb(0, 0, 0);
+            //rectArray = new Rectangle[r, cnum];
+
+            ////adds rectangles to the grid
+            //for(int i = 0; i < rows; i++)
+            //{
+            //    for(int j = 0; j < col; j++)
+            //    {
+            //        space = new Rectangle();
+            //        space.MouseDown += space_MouseDown;
+            //        rectArray[i,j] = space;
+            //        space.Fill = fill;
+            //        space.Stroke = line;
+            //        Grid.SetColumn(space, j);
+            //        Grid.SetRow(space, i);
+
+            //        gameGrid.Children.Add(space);
+            //    }
+            //}
+            
+
+        }
+        private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //handles moving pawns           
+        }
     }
 }
+
