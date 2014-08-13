a3b879a9f1a00d361765992ffbe264f3e08cb832
Chris Quintanilla <Ca.q@live.com>
2014-08-13 13:04:51 -0600

    Implementation of Enums
    This should have enums for Characters, Rooms, and weapons

---
+++ GhettoClue/GhettoClue/Model/Rooms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhettoClue.Model
+{
+    public enum Rooms
+    {
+        TheCorner,GrowHouse,BackAlley,BMommasPad,Laundrymat,
+        Prison,LightRoom,KFC,LiquorStore,TheSpot
+    }
+}
