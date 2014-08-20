f874d9613d94be39596e2601738e234c543c4183
stripealicious <gl.miller@live.com>
2014-08-20 10:04:31 -0600

    Characters
    All characters have a hand to start out with.

--- GhettoClue/GhettoClue/MainWindow.xaml.cs
+++ GhettoClue/GhettoClue/MainWindow.xaml.cs
@@ -33,28 +33,115 @@ namespace GhettoClue
 		public MainWindow()
 		{
 			InitializeComponent();
-			var randC = RandomEnumValue<Characters.CharacterCards>();
-			var randR = RandomEnumValue<Rooms.room>();
-			var randW = RandomEnumValue<Weapons.weapon>();
-			players = new List<Player>
+            #region PlayerVariables
+            var LafC = RandomEnumValue<Characters.CharacterCards>();
+			var LafR = RandomEnumValue<Rooms.room>();
+			var LafW = RandomEnumValue<Weapons.weapon>();
+
+            var DaC = RandomEnumValue<Characters.CharacterCards>();
+            var DaR = RandomEnumValue<Rooms.room>();
+            var DaW = RandomEnumValue<Weapons.weapon>();
+
+            var WatC = RandomEnumValue<Characters.CharacterCards>();
+            var WatR = RandomEnumValue<Rooms.room>();
+            var WatW = RandomEnumValue<Weapons.weapon>();
+
+            var JC = RandomEnumValue<Characters.CharacterCards>();
+            var JR = RandomEnumValue<Rooms.room>();
+            var JW = RandomEnumValue<Weapons.weapon>();
+
+            var LaC = RandomEnumValue<Characters.CharacterCards>();
+            var LaR = RandomEnumValue<Rooms.room>();
+            var LaW = RandomEnumValue<Weapons.weapon>();
+
+            var JuanC = RandomEnumValue<Characters.CharacterCards>();
+            var JuanR = RandomEnumValue<Rooms.room>();
+            var JuanW = RandomEnumValue<Weapons.weapon>();
+            #endregion
+            #region Players
+            players = new List<Player>
 			{
 				new Player{ Name = characters.Lafawnduh, background = "not yet defined", characterCards = new ObservableCollection<Characters> 
 				{
-					new Characters { character = randC.ToString()},
-					new Characters { character = randC.ToString()}
+					new Characters { character = LafC.ToString()},
 				},
 				 roomCards = new ObservableCollection<Rooms>
 				 {
-					new Rooms { location = randR.ToString()}
+					new Rooms { location = LafR.ToString()}
 				 }, 
 				  weaponCards = new ObservableCollection<Weapons>
 				  {
-					  new Weapons { leathals = randW.ToString()}
-				  }
-				}};
+					  new Weapons { leathals = LafW.ToString()}
+				  }},
+                  		new Player{ Name = characters.DaMarcus, background = "not yet defined", characterCards = new ObservableCollection<Characters> 
+				{
+					new Characters { character = DaC.ToString()},
+				},
+				 roomCards = new ObservableCollection<Rooms>
+				 {
+					new Rooms { location = DaR.ToString()}
+				 }, 
+				  weaponCards = new ObservableCollection<Weapons>
+				  {
+					  new Weapons { leathals = DaW.ToString()}
+				  }},
+                  		new Player{ Name = characters.Jake, background = "not yet defined", characterCards = new ObservableCollection<Characters> 
+				{
+					new Characters { character = JC.ToString()},
+				},
+				 roomCards = new ObservableCollection<Rooms>
+				 {
+					new Rooms { location = JR.ToString()}
+				 }, 
+				  weaponCards = new ObservableCollection<Weapons>
+				  {
+					  new Weapons { leathals = JW.ToString()}
+				  }},
+                  		new Player{ Name = characters.JuanCarlos, background = "not yet defined", characterCards = new ObservableCollection<Characters> 
+				{
+					new Characters { character = JuanC.ToString()},
+				},
+				 roomCards = new ObservableCollection<Rooms>
+				 {
+					new Rooms { location = JuanR.ToString()}
+				 }, 
+				  weaponCards = new ObservableCollection<Weapons>
+				  {
+					  new Weapons { leathals = JuanW.ToString()}
+				  }},
+                  		new Player{ Name = characters.Ladasha, background = "not yet defined", characterCards = new ObservableCollection<Characters> 
+				{
+					new Characters { character = LaC.ToString()},
+				},
+				 roomCards = new ObservableCollection<Rooms>
+				 {
+					new Rooms { location = LaR.ToString()}
+				 }, 
+				  weaponCards = new ObservableCollection<Weapons>
+				  {
+					  new Weapons { leathals = LaW.ToString()}
+				  }},
+                  		new Player{ Name = characters.Watermelondria, background = "not yet defined", characterCards = new ObservableCollection<Characters> 
+				{
+					new Characters { character = WatC.ToString()},
+				},
+				 roomCards = new ObservableCollection<Rooms>
+				 {
+					new Rooms { location = WatR.ToString()}
+				 }, 
+				  weaponCards = new ObservableCollection<Weapons>
+				  {
+					  new Weapons { leathals = WatW.ToString()}
+				  }}
+            };
+            player.ItemsSource = players;
+#endregion
+            #region Detective Notes
 
-			player.ItemsSource = players;
-		}
+            //James your stuff to fix goes here
+
+            #endregion
+        }
 
 		private T RandomEnumValue<T>()
 		{
@@ -68,12 +155,13 @@ namespace GhettoClue
 			WeaponGrid.ItemsSource = players[i].weaponCards;
 			RoomGrid.ItemsSource = players[i].roomCards;
 
-			DNotes_Characters.ItemsSource = players[i].characterCards;
-			DNotes_Weapons.ItemsSource = players[i].weaponCards;
-			DNotes_Rooms.ItemsSource = players[i].roomCards;
+            //DNotes_Characters.ItemsSource = players[i].characterCards;
+            //DNotes_Weapons.ItemsSource = players[i].weaponCards;
+            //DNotes_Rooms.ItemsSource = players[i].roomCards;
 		}
 
-		private void play_Click(object sender, RoutedEventArgs e)
+        #region Button methods
+        private void play_Click(object sender, RoutedEventArgs e)
 		{
 
 			//takes away display screen
@@ -138,8 +226,24 @@ namespace GhettoClue
 			Canvas.SetLeft(Token1, pX - 20);
 			Canvas.SetTop(Token1, pY - 30);
 
-		}
+        }
+        
 
-	}
+        private void nextTurn_Click(object sender, RoutedEventArgs e)
+        {
+            //turn taking
+        }
+
+        private void suggest_Click(object sender, RoutedEventArgs e)
+        {
+            //suggest a card
+        }
+
+        private void accuse_Click(object sender, RoutedEventArgs e)
+        {
+            //accuse the murder
+        }
+        #endregion
+    }
 }
 
