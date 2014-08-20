845504e7a5de360ce3201dd631b1035dd612186d
Chris Quintanilla <Ca.q@live.com>
2014-08-20 14:40:24 -0600

    Suggest Button Functionality
    Suggest Button now creates a pop-up window
    so we can Show it off.

---
+++ GhettoClue/GhettoClue/Converters/EnumToString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GhettoClue.Converters
+{
+    public class EnumToString: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string stringEnum =value.ToString();
+            return stringEnum;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GhettoClue/GhettoClue/SuggestionPopUpWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GhettoClue.Model;
+
+namespace GhettoClue
+{
+    /// <summary>
+    /// Interaction logic for SuggestionPopUpWindow.xaml
+    /// </summary>
+    public partial class SuggestionPopUpWindow : Window
+    {
+        public SuggestionPopUpWindow()
+        {
+            InitializeComponent();
+
+            CharacterComboBox.ItemsSource = Enum.GetValues(typeof(Characters.CharacterCards));
+            foreach (object o in CharacterComboBox.ItemsSource)
+            {
+                Console.WriteLine(o.ToString());
+            }
+            RoomComboBox.ItemsSource = Enum.GetValues(typeof (Rooms.room));
+            WeaponComboBox.ItemsSource = Enum.GetValues(typeof(Weapons.weapon));
+
+
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button confirm = (Button)sender;
+            ((Window)((StackPanel)confirm.Parent).Parent).Close();
+        }
+    }
+}
