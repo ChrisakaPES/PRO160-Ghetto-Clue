c1a67d7e5645fed7b82b1f3916bef79360185853
Chris Quintanilla <Ca.q@live.com>
2014-09-07 14:12:15 -0600

    Visual Suggestion
    This is the layout of the visual manner in which the new Suggest and
    Accuse will work so you can see things are changing lol

--- GhettoClue/GhettoClue/VisualSuggestionWindow.xaml.cs
+++ GhettoClue/GhettoClue/VisualSuggestionWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GhettoClue.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,6 +27,106 @@ namespace GhettoClue
             InitializeComponent();
             this.DataContext = this;
             
+        }
+
+        private void LaFawndaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DamarcusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JakeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LadashaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JuanCarlosButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WatermelondreaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DaHeataButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShankButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PoisonedLeanButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SmackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WeaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TheCornerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GrowHouseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackAlleyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BMommasPadButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LaundryMatButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PrisonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LightRoomButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void KFCButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LiquorStoreButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
